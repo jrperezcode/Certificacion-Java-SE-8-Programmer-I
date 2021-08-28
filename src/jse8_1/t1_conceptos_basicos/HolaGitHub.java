65469899f2a10fd2f1ae508f34f04fab60ce816e
JuniorPerezDev <junior.c.perez@gmail.com>
2021-08-27 23:49:47 -0500

    agregando gitignore y paquete tema_2

--- src/jse8_1/t1_conceptos_basicos/HolaGitHub.java
+++ src/jse8_1/t1_conceptos_basicos/HolaGitHub.java
@@ -4,5 +4,6 @@ public class HolaGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Git Hub");
+		
 	}
 }
---
+++ src/jse8_1/t2_tipos_de_datos_java/aea.java
@@ -0,0 +1,5 @@
+package jse8_1.t2_tipos_de_datos_java;
+
+public class aea {
+
+}
